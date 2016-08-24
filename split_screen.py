c4dd240d6132ebd62ba574318b84bf91f5d1fcf7
Isobel Smith <isobel@bridesmaid.org>
2016-08-24 17:11:06 +0100

    removing 'splitscreen' from the follower url

--- split_screen.py
+++ split_screen.py
@@ -21,7 +21,7 @@ def index():
 def splitscreen():
    return render_template('splitscreen.html')
 
-@app.route('/splitscreen/<regex("[0-9]{3}[-][0-9]{3}[-][0-9]{3}"):session_id>/')
+@app.route('/<regex("[0-9]{3}[-][0-9]{3}[-][0-9]{3}"):session_id>/')
 """ the regex checks whether the url contains the session id """
 def index_session_id(session_id):
    return render_template('follower_side.html')
