8f284b3bb29817b924e84f631a606ff830bc92e2
Isobel Smith <isobel@bridesmaid.org>
2016-08-24 14:34:07 +0100

    changing url to regex

--- split_screen.py
+++ split_screen.py
@@ -1,6 +1,15 @@
 import re
 from flask import Flask, request, make_response, render_template
+from werkzeug.routing import BaseConverter
 app = Flask(__name__)
+
+class RegexConverter(BaseConverter):
+    """ copied from an example -> simplify/ adjust later """
+    def __init__(self, url_map, *items):
+        super(RegexConverter, self).__init__(url_map)
+        self.regex = items[0]
+
+app.url_map.converters['regex'] = RegexConverter
 
 @app.route('/')
 def index():
@@ -10,7 +19,7 @@ def index():
 def splitscreen():
    return render_template('splitscreen.html')
 
-@app.route('/splitscreen/<session_id>')
+@app.route('/splitscreen/<regex("[0-9]{3}[-][0-9]{3}[-][0-9]{3}")/>')
 def index_session_id(session_id):
    return render_template('follower_side.html')
 
@@ -29,3 +38,5 @@ def follower_side_start():
 @app.route('/popup_link')
 def popup_link():
    return render_template('popup_link.html')
+
+
