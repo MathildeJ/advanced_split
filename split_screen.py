436dc876d8d2fe5d6d0c046d416066b0964a5950
MathildeJ <mathilde.jean@depinfonancy.net>
2016-08-25 10:39:32 +0200

    merge fix

--- split_screen.py
+++ split_screen.py
@@ -4,14 +4,12 @@ from werkzeug.routing import Rule, Map, BaseConverter, ValidationError
 app = Flask(__name__)
 
 class RegexConverter(BaseConverter):
-    #Using a converter so we can use a regex to parse the follower link url
     def __init__(self, url_map, *items):
         super(RegexConverter, self).__init__(url_map)
         self.regex = items[0]
 
 
 app.url_map.converters['regex'] = RegexConverter
-
 
 @app.route('/')
 def index():
@@ -21,15 +19,9 @@ def index():
 def splitscreen():
    return render_template('splitscreen.html')
 
-<<<<<<< HEAD
 @app.route('/<regex("[0-9]{3}[-][0-9]{3}[-][0-9]{3}"):session_id>/')
-#the regex checks whether the url contains the session id
 def index_session_id(session_id):
    return render_template('follower_side.html')
-
-@app.route('/start_page')
-def first_frame():
-   return render_template('start_page.html')
 
 @app.route('/popup_link')
 def popup_link():
