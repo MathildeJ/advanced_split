4f4f9a1a04ae259ffb43beacd7ea5c89e7f3b0d8
Isobel Smith <isobel@bridesmaid.org>
2016-08-26 16:19:46 +0100

    added copying the link

--- split_screen.py
+++ split_screen.py
@@ -7,7 +7,7 @@ from werkzeug.routing import Rule, Map, BaseConverter, ValidationError
 from flask_mandrill import Mandrill
 
 app = Flask(__name__)
-app.config['MANDRILL_API_KEY'] = 'your_key_here'
+app.config['MANDRILL_API_KEY'] = 'your key here'
 mandrill = Mandrill(app)
 
 class RegexConverter(BaseConverter):
@@ -35,13 +35,15 @@ def popup_link():
       email_address = request.form[ 'emailAddress' ]
       email_link = request.form[ 'inviteLink' ]
       mandrill.send_email(
-         from_email='isobel@surfly.com',
+         from_email='splitscreen@surfly.com',
          to=[{'email': email_address}],
-         html='<p>Hi,</p><p> You have been invited to join a splitscreen session! </p><p> Click <a href='+email_link+'>here</a> to join your friend! </p>',
+         html='<h4>You have been invited to a splitscreen session!</h4><p><a href='+email_link+'>Join the session</a></p><p><img src="cid:logo"/></p>',
          subject='Invitation to a splitscreen session',
+         images = [{'content': 'iVBORw0KGgoAAAANSUhEUgAAAFYAAAAkCAMAAAAkYj0PAAAANlBMVEXlR0b////lR0blR0blR0blR0blR0blR0blR0blR0blR0blR0blR0blR0blR0blR0blR0blR0beFbDIAAAAEXRSTlMAABAgMEBQYHCAj5+vv8/f7/4ucL8AAALfSURBVHjatZbZttsgDEV9AYGxmfz/P1uhATwkadrV8hAbkLeEjoAsPz8/y181u+YDW03hNkHIiTVkV6L5ihqJ2fpvMR+wrh7cmvuWWhZHj/Aea8lxtAtsX3CBArD4Ar37HpuJynGnT8Rp3c0gplwO9w5ryL24Xu0H4rRelX+8jRbOWIPPvr7AwYMnNxb7BiKgQJ6tcZhSl9VuYTuAK3YTf26pYr5qNKX3QyNJk6gbFqufJbFz7PCahJEkczEvMoBQ6m70qKivF/wtDDMk2y/FBRfzpAPNEnFpqljkYG5h1HOBDe7DPOpA4Kfl0aGYel016tN28HMzjFRNJXea44nkZXQqFnlEwzhv3nCIECLQUdTc4AB/b8ixjk7F9ksYnrFPLj13TRXOybJ9LbtbsoyqYvcwLGMfXNsuqZoSUnsodgujyXZ4cl2jVIFqs553iyg2JRDsJjP5hDWAzSBucqMtnKqROxFXRjXjHcvcyg4VC3uVSvdFuByoHFQzm7P67AhyhK1rZazLvGsytgZiAZqQmbusx5cU20zpIqus7JCwgQb01HK63yw9ac46XtxZsUxv4yQzIcbogBXrWC9fzLZyPkw99sJv4VwIdvqIp405+omwVW1mS5Je3Lxr6ldg2OKKgsp69hgBeHE+5WhvH64dC93f9Y7TOjHfXJaaOronK0mh2Hy3qoxtu/0KanJc+v1ruF461nUtn1cVjcaGJ/fvqb5hFrsEjutNc1teRgsYBoLb9vkitpnEGdJaxgIRnrlNvL5QqfreQnepC/4k9Gr70bqtV3H2y1+V0MOpG7zIaZ9SLOfNzZs34Er9cj1w0tmR2+lqTBFG1P0O7syJFe7E0krK6tjc9w484ioCKJma9CZWtr+fWPqsb6hG5uW1RnYtZ9ATm7T+J3b8bxjLfE1Ob7EtYP3EC/ZPGqz45+PekInhBAz3b7CTDTFulGM8HwKovPUq2b9qcGz/A4uZEOwvoTZPQ1Kv5d4AAAAASUVORK5CYII=',
+                    'name': 'logo',
+                    'type': 'image/png'}],
          )
       return render_template('fourth_frame.html')
    else:
       return render_template('popup_link.html')
-
 
